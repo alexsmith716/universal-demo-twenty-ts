acd5e48970379eff0eeef65924f65ed57746a67d
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2020-06-23 21:39:31 -0400

    feat: Added module LinkTest to jestTest to use as example for jest snapshot testing.

---
+++ src/components/jestTest/LinkTest.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+
+//	https://github.com/facebook/jest/blob/master/docs/TutorialReact.md
+
+const LinkTest = (props) => {
+
+	const { page, children } = props;
+	const [onMouseHoverStatus, setOnMouseHoverStatus] = useState('normal');
+
+	useEffect(() => {
+			// componentDidMount
+			// console.log('>>>>>>>>>>>>>> LinkTest > useEffect() > componentDidMount > onMouseHoverStatus: ', onMouseHoverStatus);
+
+			// componentDidUpdate
+			if (onMouseHoverStatus) {
+				// console.log('>>>>>>>>>>>>>> LinkTest > useEffect() > componentDidUpdate > onMouseHoverStatus: ', onMouseHoverStatus);
+			}
+
+			// componentWillUnmount
+			return () => {
+				// some effects might require cleanup
+				// console.log('>>>>>>>>>>>>>> LinkTest > useEffect() > componentWillUnmount > cleanup phase');
+			};
+		},
+		[onMouseHoverStatus] // only re-run the effect if an array item changes
+	);
+
+	return (
+
+		<div className="container">
+
+			<h2>LinkTest</h2>
+
+			<div className="row">
+				<div className="col-lg-12 mb-4">
+					<div className="bg-color-ivory container-padding-border-radius-1 text-break">
+
+						<div>
+							{onMouseHoverStatus && (
+								<p>
+									state variable "onMouseHoverStatus" state: {onMouseHoverStatus}
+								</p>
+							)}
+						</div>
+
+						<div>
+							<a
+								className={onMouseHoverStatus}
+								href={page || '#'}
+								onMouseEnter={() => setOnMouseHoverStatus('hovered')}
+								onMouseLeave={() => setOnMouseHoverStatus('normal')}
+							>
+								{children}
+							</a>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+		</div>
+	);
+}
+
+export default LinkTest;
