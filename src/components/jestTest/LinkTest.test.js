6c34651dd3749eaf6de1d7044c6438b969eb56cf
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2020-06-25 23:32:21 -0400

    feat: Created describe block for jestTest LinkTest test and added basic schema stitching ideas to README.

--- src/components/jestTest/LinkTest.test.js
+++ src/components/jestTest/LinkTest.test.js
@@ -1,25 +1,25 @@
 
 import React from 'react';
 import { render, getAllByTestId, fireEvent } from '@testing-library/react';
-// import { renderHook } from '@testing-library/react-hooks'
 import LinkTest from './LinkTest';
 
-test('Link changes the class when hovered', () => {
+describe('LinkTest', () => {
+	test('Link changes the class when hovered', () => {
 
-	const { container } = render(
-		<LinkTest page='https://github.com/'>Github</LinkTest>
-	);
+		const { container } = render(
+			<LinkTest page='https://github.com/'>Github</LinkTest>
+		);
 
-	expect(container).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 
-	const [element] = getAllByTestId(container, 'hover-anchorlink');
+		const [element] = getAllByTestId(container, 'hover-anchorlink');
 
-	fireEvent.mouseEnter(element)
+		fireEvent.mouseEnter(element)
 
-	expect(container).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 
-	fireEvent.mouseLeave(element)
+		fireEvent.mouseLeave(element)
 
-	expect(container).toMatchSnapshot();
-
+		expect(container).toMatchSnapshot();
+	});
 });
